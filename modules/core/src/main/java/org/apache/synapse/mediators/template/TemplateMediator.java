433434b53aa6fd36a6676ec2b4cb2268d62a56d0
Dilan Tharaka <tharakamd.12@cse.mrt.ac.lk>
2021-01-06 15:40:25 +0530

    add initializing flag to TemplateMediator

--- modules/core/src/main/java/org/apache/synapse/mediators/template/TemplateMediator.java
+++ modules/core/src/main/java/org/apache/synapse/mediators/template/TemplateMediator.java
@@ -50,6 +50,8 @@ public class TemplateMediator extends AbstractListMediator {
     private String artifactContainerName;
     /** whether the template edited via the management console or not */
     private boolean isEdited;
+    /** flag to identify whether the mediator is currently initializing */
+    private boolean initializing = false;
 
     private String errorHandler = null;
 
@@ -193,8 +195,10 @@ public class TemplateMediator extends AbstractListMediator {
     @Override
     public synchronized void init(SynapseEnvironment se) {
         if (!initialized) {
+            initializing = true;
             super.init(se);
             initialized = true;
+            initializing = false;
             if (!isDynamic()) {
                 // mark as available, if this is marked previously as unavailable in the environment
                 se.clearUnavailabilityOfArtifact(eipPatternName);
@@ -214,6 +218,9 @@ public class TemplateMediator extends AbstractListMediator {
         return initialized;
     }
 
+    public boolean isInitializing(){
+        return initializing;
+    }
     /**
      * Is this a dynamic template?
      *
