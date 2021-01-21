55ecd124c53173eb0ddba74c34b9bb026a39bbde
Indika Sampath <indikasampath2000@gmail.com>
2021-01-21 11:12:38 +0530

    Fix JsonStreamFormatter error while sending GET request with messageType property
    Resolves: https://github.com/wso2/product-ei/issues/5348

--- modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/passthru/PassThroughHttpSender.java
+++ modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/passthru/PassThroughHttpSender.java
@@ -377,11 +377,9 @@ public class PassThroughHttpSender extends AbstractHandler implements TransportS
 
             if ("true".equals(disableChunking) || "true".equals(forceHttp10)) {
 
-                long messageSize;
-
                 try {
                     OverflowBlob overflowBlob = setStreamAsTempData(formatter, msgContext, format);
-                    messageSize = overflowBlob.getLength();
+                    long messageSize = overflowBlob.getLength();
                     msgContext.setProperty(PassThroughConstants.PASSTROUGH_MESSAGE_LENGTH, messageSize);
                     deliveryAgent.submit(msgContext, epr);
                     if (!waitForReady(msgContext)) {
@@ -389,6 +387,10 @@ public class PassThroughHttpSender extends AbstractHandler implements TransportS
                     }
                     out = (OutputStream) msgContext.getProperty(PassThroughConstants.BUILDER_OUTPUT_STREAM);
                     if (out != null) {
+                        // Check HTTP method is GET or DELETE with no body
+                        if (ignoreMessageBody(msgContext, pipe)){
+                            return;
+                        }
                         overflowBlob.writeTo(out);
                         if (pipe.isStale) {
                             throw new IOException("Target Connection is stale..");
@@ -405,6 +407,10 @@ public class PassThroughHttpSender extends AbstractHandler implements TransportS
                 }
                 out = (OutputStream) msgContext.getProperty(PassThroughConstants.BUILDER_OUTPUT_STREAM);
                 if (out != null) {
+                    // Check HTTP method is GET or DELETE with no body
+                    if (ignoreMessageBody(msgContext, pipe)) {
+                        return;
+                    }
                     formatter.writeTo(msgContext, format, out, false);
                     if (pipe.isStale) {
                         handleException("IO while building message", new IOException("Target Connection is stale.."));
@@ -415,6 +421,17 @@ public class PassThroughHttpSender extends AbstractHandler implements TransportS
         } else {
             deliveryAgent.submit(msgContext, epr);
         }
+    }
+
+    // If the HTTP method is GET or DELETE with no body, we need to write down the HEADER information to the wire
+    // and need to ignore any entity enclosed methods available.
+    private boolean ignoreMessageBody(MessageContext msgContext, Pipe pipe) {
+        if (HTTPConstants.HTTP_METHOD_GET.equals(msgContext.getProperty(Constants.Configuration.HTTP_METHOD)) ||
+                RelayUtils.isDeleteRequestWithoutPayload(msgContext)) {
+            pipe.setSerializationComplete(true);
+            return true;
+        }
+        return false;
     }
 	
 	
