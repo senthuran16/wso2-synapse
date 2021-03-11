0d9311571033f70bbf67cb3519b9c2fccafe6614
Vathsan <srivathsanmail@gmail.com>
2021-03-11 12:08:00 +0530

    Fix review comments

--- modules/core/src/main/java/org/apache/synapse/mediators/GetPropertyFunction.java
+++ modules/core/src/main/java/org/apache/synapse/mediators/GetPropertyFunction.java
@@ -56,7 +56,15 @@ public class GetPropertyFunction implements Function , XPathFunction {
 
     public static final String NULL_STRING = "";
 
-    public static Map propertyMap = FilePropertyLoader.getPropertyMap();
+    public static Map filePropertyMap;
+
+    static {
+        filePropertyMap = FilePropertyLoader.getPropertyMap();
+        if (filePropertyMap == null) {
+            FilePropertyLoader propertyLoader = FilePropertyLoader.getInstance();
+            filePropertyMap = propertyLoader.getPropertyMap();
+        }
+    }
 
     /** Synapse Message context*/
     private final MessageContext synCtx;
@@ -366,13 +374,9 @@ public class GetPropertyFunction implements Function , XPathFunction {
             }
 
         } else if (XMLConfigConstants.SCOPE_FILE.equals(scope)) {
-            if (propertyMap == null) {
-                FilePropertyLoader propertyLoader = FilePropertyLoader.getInstance();
-                propertyMap = propertyLoader.getPropertyMap();
-            }
-            String propVal = (String) propertyMap.get(key);
+            Object propVal = filePropertyMap.get(key);
             if (propVal != null) {
-                return propVal;
+                return (String) propVal;
             } else {
                 if (traceOrDebugOn) {
                     traceOrDebug(traceOn, "Property " + key + " not found in properties file");
